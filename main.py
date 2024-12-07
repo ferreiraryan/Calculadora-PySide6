0ce7d6e774e78da8d0af3cd2ec89b318efbea403
2Ryck5 <ryck342020@hotmail.com>
2024-12-06 21:52:58 -0300

    Compacting code

--- main.py
+++ main.py
@@ -1,12 +1,107 @@
 import sys
 
-from main_window import MainWindow
-from display import Display
-from info import Info
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon, QKeyEvent
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QLineEdit, QMainWindow, QVBoxLayout, QMessageBox
+from PySide6.QtCore import Qt, Signal
 from styles import setupTheme
-from variables import WINDOW_ICON_PATH
+from buttons import Button, ButtonsGrid
+from variables import WINDOW_ICON_PATH, SMALL_FONT_SIZE, BIG_FONT_SIZE, TEXT_MARGIN, MINIMUM_WIDTH
+from utils import isEmpty, isNUmOrDot
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
+        super().__init__(parent,*args, **kwargs)
+        
+        # Config layout
+        self.cw = QWidget()
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
+        self.setCentralWidget(self.cw)
+        
+        # Set title
+        self.setWindowTitle("Calculadora")
+        
+
+        
+        
+    def adjustFixedSize(self):
+        #Last ajust
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
+        
+    def addWidgetToVLayout(self, widget: QWidget):
+        self.vLayout.addWidget(widget)
+        
+    def makeMsgBox(self):
+        return QMessageBox(self)
+
+
+class Info(QLabel):
+    def __init__(self, text: str, parent: QWidget | None = None) -> None:
+        super().__init__(text, parent)
+        self.configStyles()
+        
+    def configStyles(self):
+        self.setStyleSheet(f'font-size: {SMALL_FONT_SIZE}px;')
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+class Display(QLineEdit):
+    eqPressed = Signal()
+    delPressed = Signal()
+    clearPressed = Signal()
+    inputPressed = Signal(str)
+    operatorPressed = Signal(str)
+    
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configStyle()
+    
+    def configStyle(self):
+        margins = [TEXT_MARGIN for _ in range(4)]
+        self.setStyleSheet(f'font-size:{BIG_FONT_SIZE}px;')
+        self.setMinimumHeight(BIG_FONT_SIZE * 2)
+        self.setMinimumWidth(MINIMUM_WIDTH)
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.setTextMargins(*margins)
+        
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        text = event.text().strip()
+        key = event.key()
+        KEYS = Qt.Key
+        
+        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return, KEYS.Key_Equal]
+        isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete, KEYS.Key_D]
+        isEsc = key in [KEYS.Key_Escape, KEYS.Key_C]
+        isOperator = key in [
+            KEYS.Key_Plus, KEYS.Key_Minus, KEYS.Key_Slash, KEYS.Key_Asterisk,
+            KEYS.Key_P
+            ]
+        
+        if isEnter:
+            self.eqPressed.emit()
+            return event.ignore()
+        if isDelete:
+            self.delPressed.emit()
+            return event.ignore()
+        if isEsc:
+            self.clearPressed.emit()
+            return event.ignore()
+        if isOperator:
+            if text.lower() == 'p':
+                text = '^'
+            self.operatorPressed.emit(text)
+            return event.ignore()
+        #Não passar daqui se não tiver texto
+        if isEmpty(text):
+            return event.ignore()
+        
+        if isNUmOrDot(text):
+            self.inputPressed.emit(text)
+            return event.ignore()
+
+
 
 if __name__ == '__main__':
     # Make the aplication
@@ -20,12 +115,17 @@ if __name__ == '__main__':
     app.setWindowIcon(icon)
     
     # Info
-    info = Info('2.0 ^ 10.0 = 1024')
-    window.addToVLayout(info)
+    info = Info('sua conta')
+    window.addWidgetToVLayout(info)
     
     # Display
     display = Display()
-    window.addToVLayout(display)
+    window.addWidgetToVLayout(display)
+    
+    # Grid
+    buttonsGrid = ButtonsGrid(display, info,window)
+    window.vLayout.addLayout(buttonsGrid)
+
     
     window.show()
     app.exec()
